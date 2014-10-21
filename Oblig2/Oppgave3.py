d36307d1f63ecf27df55360a234ff29388a96843
Schoyen <oyvindschoyen@gmail.com>
2014-10-21 17:23:05 +0200

    Shining up oppgave 2

--- Oblig2/Oppgave3.py
+++ Oblig2/Oppgave3.py
@@ -2,17 +2,31 @@ from Oppgave2 import ParticleInMagneticField
 from numpy import array, cos, linalg, cross, zeros, linspace
 from matplotlib.pylab import plot, show, title, legend, xlabel, ylabel, savefig
 
+"""
+Subclass of subclass ParticleInMagneticField.
+"""
 class Cyclotron(ParticleInMagneticField):
 
+    """
+    Constructor storing most values in parentclass which in turn stores these 
+    in the superclass.
+    """
     def __init__(self, omega, E, B, m, q, r0, v0, t_start, t_final, dt):
-        ParticleInMagneticField.__init__(self, E, B, m, q, r0, v0, t_start, t_final, dt)
-        self.omega = omega
+        ParticleInMagneticField.__init__(self,\
+                E, B, m, q, r0, v0, t_start, t_final, dt)
+        self.omega = omega # Angular velocity.
 
+    """
+    Method overriding ParticleInMagneticField.calculatePath().
+    """
     def calculatePath(self):
 
         for i in range(self.n):
+            # Conditions for calculating the force with the electric field.
+            # If this condition is fulfilled the particle is between the dee's.
             if -0.1 <= self.r[i, 0] <= 0.1:
-                F = q*(cross(self.v[i, :], self.B) + array([cos(self.omega*self.t[i]), 0, 0]))
+                F = q*(cross(self.v[i, :], self.B)\
+                        + array([cos(self.omega*self.t[i]), 0, 0]))
             else:
                 F = q*cross(self.v[i, :], self.B)
 
@@ -20,12 +34,17 @@ class Cyclotron(ParticleInMagneticField):
             self.v[i+1, :] = self.v[i, :] + a*self.dt
             self.r[i+1, :] = self.r[i, :] + self.v[i+1, :]*self.dt
 
+    """
+    Method calculating the escape velocity of the particle when it 
+    reaches the "exit".
+    """
     def escapeVelocity(self, rD):
 
         for i in range(self.n+1):
             if rD - linalg.norm(self.r[i, :]) <= 0:
                 self.vesc = linalg.norm(self.v[i, :])
                 finalTime = i
+                # Avoiding arrays of length n+1.
                 break
 
         tempr = zeros(shape=(finalTime, 3))
@@ -42,6 +61,9 @@ class Cyclotron(ParticleInMagneticField):
         self.t = tempt
         print ("Escape velocity of proton: v = %g" % self.vesc)
 
+    """
+    Method used for plotting of a particle in a cyclotron.
+    """
     def plotCPath(self, TITLE):
         
         plot(self.r[:, 0], self.r[:, 1])
